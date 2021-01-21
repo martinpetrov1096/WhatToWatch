fe7750e4f886a3bd05b582d5afda6b2855a2ef00
Martin Petrov <mlpetrov@ucdavis.edu>
2021-01-20 21:30:19 -0800

    added backend manually for now

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import config from './config/config.json';
+import path from 'path';
 import gameRouter from './routes/gameRoute';
 import detailsRouter from './routes/detailsRoute';
 import { errors } from 'celebrate';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-
 import { lobbySocket } from './routes/sockets/lobbySocket';
 import { gameSocket } from './routes/sockets/gameSocket';
 import gameMiddleware from './middleware/gameMiddleware';
@@ -15,9 +15,13 @@ import gameMiddleware from './middleware/gameMiddleware';
 ///////////////////////////////////////////////////////////////////////////
 
 const app = express();
+
+
 app.use(bodyParser.json());
 app.use('/game', gameRouter);
 app.use('/details', detailsRouter);
+
+app.use(express.static(path.join(__dirname, './build')));
 app.use(errors());
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////// SOCKET IO CONFIG ////////////////////////////
@@ -31,6 +35,10 @@ const lobbyIO = io.of('/lobby');
 const gameIO = io.of('/game');
 lobbyIO.on('connection', lobbySocket);
 gameIO.on('connection', gameSocket);
+
+
+
+
 
 httpServer.listen(config.port, () => {
    console.log('what-to-watch listening on port ' + config.port);
