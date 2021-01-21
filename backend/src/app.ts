fb2e47a739c9ccc37dabe81fd03348b001cb23f0
Martin Petrov <mlpetrov@ucdavis.edu>
2021-01-20 22:33:38 -0800

    removed period

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use('/game', gameRouter);
 app.use('/details', detailsRouter);
 
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, '/build')));
 app.use(errors());
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////// SOCKET IO CONFIG ////////////////////////////
