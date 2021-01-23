98c924a9d9ac8eb00b7f2ea1010572af7876e0d7
Martin Petrov <mlpetrov@ucdavis.edu>
2021-01-22 17:18:45 -0800

    changed db to be env variable instead

--- backend/src/services/lobbyService.ts
+++ backend/src/services/lobbyService.ts
@@ -15,7 +15,7 @@ export class LobbyService {
    private setAsync: any;
 
    private constructor() {
-      this.client = redis.createClient(config.db.port, config.db.ip);
+      this.client = redis.createClient(parseInt(process.env.DB_PORT ?? ''), process.env.DB_IP);
       this.redlock = new Redlock([this.client], {
          retryDelay: 50,
          retryCount: 40,
