2529debbe6a97aedbdf2f1386555a7b3d6c28f1a
Martin Petrov <martinlpetrov1@gmail.com>
2022-01-23 22:06:20 -0800

    [FIX] Fixing typo

--- backend/src/middleware/gameMiddleware.ts
+++ backend/src/middleware/gameMiddleware.ts
@@ -1,6 +1,6 @@
 export default async(socket: any, next: (err?: any) => void): Promise<void> => {
 
    if (!socket.handshake.query.gameId) {
-      next(new Error('Socket miissing gameId param'));
+      next(new Error('Socket missing gameId param'));
    }
 }
